e822980f61251e78a35efee096660c38d043d856
mschae23 <46165762+mschae23@users.noreply.github.com>
2022-10-22 23:32:15 +0200

    Fixed config field "cost_config" -> "cost_function"

--- src/main/java/de/martenschaefer/grindenchantments/config/MoveConfig.java
+++ src/main/java/de/martenschaefer/grindenchantments/config/MoveConfig.java
@@ -10,7 +10,7 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 public record MoveConfig(boolean enabled, CostFunction costFunction) {
     public static final Codec<MoveConfig> CODEC = RecordCodecBuilder.create(instance -> instance.group(
         Codec.BOOL.fieldOf("enabled").forGetter(MoveConfig::enabled),
-        CostFunction.TYPE_CODEC.fieldOf("cost_config").forGetter(MoveConfig::costFunction)
+        CostFunction.TYPE_CODEC.fieldOf("cost_function").forGetter(MoveConfig::costFunction)
     ).apply(instance, instance.stable(MoveConfig::new)));
 
     public static final MoveConfig DEFAULT = new MoveConfig(true,
